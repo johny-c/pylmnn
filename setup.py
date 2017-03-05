3d647152f5c9699f88b4784c26a75648a7bd5f2f
John Chiotellis <chiotell@in.tum.de>
2017-03-05 18:02:15 +0100

    add GPy req in setup.py

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ if __name__ == '__main__':
         install_requires=['numpy>=1.11',
                           'scipy>=0.18',
                           'scikit_learn>=0.18',
+			  'GPy>=1.5.6',
                           'GPyOpt>=1.0.3',
                           'matplotlib>=1.5.3'],
     )
