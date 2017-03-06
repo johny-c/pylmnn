4717f8bc218e8cec25aefb84d9cee867bf58c710
John Chiotellis <chiotell@in.tum.de>
2017-03-06 13:01:46 +0100

    latest docs mock modules in conf.py for autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,8 @@ import mock
 
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'sklearn',
         'GPyOpt', 'GPyOpt.methods', 'matplotlib.axes', 'sklearn.metrics',
-        'mpl_toolkits.mplot3d']
+        'sklearn.metrics.pairwise',
+        'mpl_toolkits.mplot3d', 'sklearn.neighbors', 'numpy.linalg', ]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
