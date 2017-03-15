776d7128158574254ea548e8584c252e246796b0
John Chiotellis <chiotell@in.tum.de>
2017-03-15 21:01:39 +0100

    add mock modules in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 # For autodoc to produce the source code in readthedocs.io
 import mock
