5d9f95d5b678d021a6b4b85f7040a7c0adfd0d86
John Chiotellis <chiotell@in.tum.de>
2017-03-16 00:06:05 +0100

    v1.4.0 try docs

--- pylmnn/tests/test_sth.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-import pylmnn
-
-
-class TestJoke(TestCase):
-    def test_is_string(self):
-        s = pylmnn.__version__
-        self.assertTrue(isinstance(s, basestring))
