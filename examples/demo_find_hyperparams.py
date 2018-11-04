b3cdbaa34493f394bfeb666860f880aef5552124
John Chiotellis <johnyc.code@gmail.com>
2018-11-04 17:35:01 +0100

    move some profiling runs under benchmark, add demo_find_hyperparams

---
+++ examples/demo_find_hyperparams.py
@@ -0,0 +1,10 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+from pylmnn import find_hyperparams
+
+
+X, y = load_iris(return_X_y=True)
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.4, stratify=y, random_state=42)
+best_params = find_hyperparams(X_train, y_train, X_valid, y_valid, params={}, max_bopt_iter=5)
+best_n_neighbors, best_n_neighbors_predict, best_n_components, best_max_iter = best_params
--- tests/test_bayesopt.py
+++ tests/test_bayesopt.py
@@ -8,4 +8,3 @@ def test_find_hyperparams():
     X, y = load_iris(return_X_y=True)
     X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.4, stratify=y, random_state=42)
     best_params = find_hyperparams(X_train, y_train, X_valid, y_valid, params={}, max_bopt_iter=5)
-
